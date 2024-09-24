b98680cafc37892786040e355369f7ec6cf6a946
BernardoLA <BernardoLA>
2024-09-24 09:26:49 +0000

    added common functions and configuration notebooks and adjusted results ingestion
    There were some changes changes made in the merge function to account for unity catalog. Managed tables can no longer use the spark folder path due to new structure. We have to use the spark table structure catalog.schema.table.

---
+++ includes/configuration.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+# create variables for paths of containers
+raw_folder_path = "/mnt/formulaonedb/raw"
+processed_folder_path = "/mnt/formulaonedb/processed"
+presentation_folder_path = "/mnt/formulaonedb/presentation"
+
+# COMMAND ----------
+
+
